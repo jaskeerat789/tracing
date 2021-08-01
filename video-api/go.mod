1bac532f4eaea32ebd413a8af2307bdb313ebfd4
Jaskeerat Singh <jaskee789@gmail.com>
2021-08-01 23:47:28 +0530

    video-api: init

---
+++ video-api/go.mod
@@ -0,0 +1,14 @@
+module video-api
+
+go 1.16
+
+require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/uber/jaeger-client-go v2.29.1+incompatible
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+)
---
+++ video-api/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/julienschmidt/httprouter"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+)
+
+const serviceName = "video-api"
+
+var environment = os.Getenv("ENVIRONMENT")
+var redis_host = os.Getenv("REDIS_HOST")
+var redis_port = os.Getenv("REDIS_PORT")
+var jaeger_host_port = os.Getenv("JAEGER_HOST_PORT")
+var flaky = os.Getenv("FLAKY")
+var delay = os.Getenv("DELAY")
+
+var rdb *redis.Client
+
+func main() {
+	cfg := config.Configuration{
+		ServiceName: serviceName,
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: jaeger_host_port,
+		},
+	}
+
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+	}
+	defer closer.Close()
+	opentracing.SetGlobalTracer(tracer)
+
+	router := httprouter.New()
+
+	router.GET("/:id", func(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+		spanCtx, _ := tracer.Extract(
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(r.Header),
+		)
+
+		span := tracer.StartSpan("video-api: GET /id", ext.RPCServerOption(spanCtx))
+		defer span.Finish()
+
+		if flaky == "true" {
+			if rand.Intn(90) < 30 {
+				panic("flaky error occurred")
+			}
+		}
+
+		ctx := opentracing.ContextWithSpan(context.Background(), span)
+		video := getVideo(rw, r, p, ctx)
+
+		if strings.Contains(video, "jM36M39MA3I") && delay == "true" {
+			time.Sleep(6 * time.Second)
+		}
+
+		cors(rw)
+		fmt.Fprint(rw, "%s", video)
+
+	})
+
+	r := redis.NewClient(&redis.Options{
+		Addr: redis_host + ":" + redis_port,
+		DB:   0,
+	})
+
+	rdb = r
+
+	fmt.Println("Running")
+	log.Fatal(http.ListenAndServe(":10010", router))
+}
+
+func getVideo(wr http.ResponseWriter, r *http.Request, p httprouter.Params, ctx context.Context) (response string) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "video-api: redis=get")
+	defer span.Finish()
+	id := p.ByName("id")
+
+	videoData, err := rdb.Get(ctx, id).Result()
+	if err == redis.Nil {
+		span.Tracer().Inject(
+			span.Context(),
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(r.Header),
+		)
+		return "{}"
+	} else if err != nil {
+		panic(err)
+	} else {
+		span.Tracer().Inject(
+			span.Context(),
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(r.Header),
+		)
+		return videoData
+	}
+
+}
+
+func cors(writer http.ResponseWriter) {
+	if environment == "DEBUG" {
+		writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-MY-API-Version")
+		writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		writer.Header().Set("Access-Control-Allow-Origin", "*")
+	}
+}
